9337e12b88f2334874ed8772487bf480e4b5147e
Valentino Ekaputra <silentrazgriz@gmail.com>
2018-06-24 21:09:36 +0700

    change responsive

--- dist/bundle.js
+++ dist/bundle.js
@@ -73,6 +73,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__scss_app_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__scss_app_scss__);
 
 
+
 /***/ }),
 /* 1 */
 /***/ (function(module, exports) {

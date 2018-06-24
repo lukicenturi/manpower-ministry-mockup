206f6a0ee0a48020c23fd8e95e0031e91990f878
Valentino Ekaputra <silentrazgriz@gmail.com>
2018-06-24 20:22:59 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000
+                }
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css')
+    ]
+};
